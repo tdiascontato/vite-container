911a708c45801d3d3d575cc0d7b78f43d6a5fb60
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-08-06 12:56:01 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
+import {useState} from 'react';
 import { Outlet } from 'react-router-dom';
-
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import global_pt from './translate/pt/global.json';
+import global_en from './translate/en/global.json';
 import Footer from './Components/footer';
 import NavBar from './Components/navbar';
 import './Styles/App.css';
 
 function App() {
 
+  const [language, setLanguage] = useState('pt');
+  const toggleLanguage = () => {
+    language === 'pt' ? setLanguage('en') : setLanguage('pt')
+  }
+
+  i18next.init({
+    interpolation: {escapeValue: false},
+    lng: `${language}`,
+    resources:{
+      pt: {
+        global: global_pt
+      },
+      en: {
+        global: global_en
+      }
+    }
+  })
+
   return (
+    <I18nextProvider i18n={i18next}>
     <div className="ContainerApp">
 
       <div className='NavBar'>
-        <NavBar />
+        <NavBar modeLanguage={toggleLanguage}/>
       </div>
       <div className='Outlet'>
         <Outlet/>
@@ -20,6 +43,7 @@ function App() {
       </div>
 
     </div>
+    </I18nextProvider>
   )
 }
 
--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,42 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 import '../Styles/NavBar.css';
-import Wapp from '../Img/whatsapp.png';
 import Logo from '../Img/logo.png';
-import github from '../Img/github.png';
-import linkedin from '../Img/linkedin.png';
-import facebook from '../Img/facebook.png';
 import languages from '../Img/languages.png';
 import mode from '../Img/mode.png';
 
-export default function NavBar() {
+export default function NavBar({modeLanguage}) {
+  const {t} = useTranslation("global");
+
   return (
     <div className='NavBarContainer'>
       <ul className="Box-Menu">
-          <li><Link to ='https://tdiascontato.vercel.app/' className='link Home'>Home</Link></li>
-          <li><Link to ='https://tdiascontato.vercel.app/repository' className='link'>Repository</Link></li>
+        <li><Link to="/"><img src={Logo}  alt= 'Logo' className='Logo' /></Link></li>
+        <li><Link to ='https://tdiascontato.vercel.app/repository' className='link'>{t("NavBar.Works")}</Link></li>
       </ul>
-                          
-      <Link to="https://tdiascontato.vercel.app/"><img src={Logo}  alt= 'Logo' className='Logo' /></Link>
                 
-      <div className="RightSide">
-
-        <ul className="BoxSocial">
-            <li><Link to = "https://api.whatsapp.com/send?phone=5521990032507&text=Ol%C3%A1!%0AComo%20vai?" target="_blank">
-              <img src={Wapp} alt = 'Meu wahtsapp' /></Link></li>
-            <li><Link to = "http://www.github.com/tdiascontato" target="_blank">
-              <img src={github} alt = 'Meu github' /></Link></li>
-            <li><Link to = "http://www.linkedin.com/in/tdiascontato" target="_blank">
-              <img src={linkedin} alt = 'Meu Linkedin' /></Link></li>
-            <li><Link to = "http://www.facebook.com/aulascomtiago" target="_blank">
-              <img src={facebook} alt = 'Meu Facebook' /></Link></li>
-        </ul>         
-                  
+      <div className="RightSide">                  
         <ul className="BoxCheats">
-            <li><img src={languages} alt = 'pt-br'/></li>
+            <li><img src={languages} onClick={modeLanguage} alt = 'pt-br'/></li>
             <li><img src={mode} alt = 'light-dark'/></li>
         </ul>
-              
      </div>
     </div>
   )
